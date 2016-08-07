0ea405833765b32a1322b3c9db0deee986ef99e0
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-08-07 17:53:35 -0400

    changesyes

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name,devoured,date)
+VALUES ("Classic Hamburger",false);
+
+INSERT INTO burgers (burger_name,devoured,date)
+VALUES ("Cheese Burger",false);
+
+INSERT INTO burgers (burger_name,devoured,date)
+VALUES ("Triple Bacon Cheese Burger",false);
